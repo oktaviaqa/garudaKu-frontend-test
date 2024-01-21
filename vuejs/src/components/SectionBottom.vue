2d6774548b197c61fc508672f9ad13aa8511705e
oktaviaqa <oktaviaq.a@gmail.com>
2024-01-21 09:30:19 +0700

    add section bottom left in homeview

---
+++ vuejs/src/components/SectionBottom.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container-bottom-left">
+        <article>
+            <img src="images/bottom-left-1.jpg" />
+            <div>
+                <h3>Best smart speakers for the year</h3>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Commodi iure modi animi cupiditate. Explicabo, nihil?
+                </p>
+
+                <a href="#">Read More <span>>></span></a>
+            </div>
+        </article>
+
+        <article>
+            <img src="images/bottom-left-2.jpg" />
+            <div>
+                <h3>
+                    iPad Pro, reviewed: Has the iPad become my main computer now?
+                </h3>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Commodi iure modi animi cupiditate. Explicabo, nihil?
+                </p>
+
+                <a href="#">Read More <span>>></span></a>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- vuejs/src/components/SectionLeftMain.vue
+++ vuejs/src/components/SectionLeftMain.vue
@@ -19,41 +19,16 @@
                 </div>
             </article>
         </div>
+        <SectionBottom />
 
-        <div class="container-bottom-left">
-            <article>
-                <img src="images/bottom-left-1.jpg" />
-                <div>
-                    <h3>Best smart speakers for the year</h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Commodi iure modi animi cupiditate. Explicabo, nihil?
-                    </p>
-
-                    <a href="#">Read More <span>>></span></a>
-                </div>
-            </article>
-
-            <article>
-                <img src="images/bottom-left-2.jpg" />
-                <div>
-                    <h3>
-                        iPad Pro, reviewed: Has the iPad become my main computer now?
-                    </h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Commodi iure modi animi cupiditate. Explicabo, nihil?
-                    </p>
-
-                    <a href="#">Read More <span>>></span></a>
-                </div>
-            </article>
-        </div>
     </section>
 </template>
 
 <script>
+import SectionBottom from './SectionBottom.vue';
 export default {
-
+    components: {
+        SectionBottom
+    }
 }
 </script>
